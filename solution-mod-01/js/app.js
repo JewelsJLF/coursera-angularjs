1eefdc708b5dc283fd582a87a87a18cdb211e0d6
Julie Farkas <julie.l.farkas@gmail.com>
2016-12-09 14:48:24 -0700

    Solution complete.

--- solution-mod-01/js/app.js
+++ solution-mod-01/js/app.js
@@ -5,10 +5,21 @@ angular.module('LunchCheck', [])
 
   .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
-  
-  function LunchCheckController($scope) {
+LunchCheckController.$inject = ['$scope'];
 
-  }
+function LunchCheckController($scope) {
+  $scope.lunchItems = '';
+  $scope.message = '';
+  $scope.checkIfTooMuch = function(){
+    if ($scope.lunchItems){
+      var numItems = $scope.lunchItems.split(',').length;
+      console.log(numItems);
+      if (numItems > 3) $scope.message = 'Too much!';
+      else if (numItems > 0) $scope.message = 'Enjoy!';
+    }
+    else $scope.message = "Please enter data first.";
+  };
+
+}
 
 })();
