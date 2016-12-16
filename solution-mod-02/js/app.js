b8c9e9bec648bbb06aea5c41bcb124c32b77ee3c
Julie Farkas <julie.l.farkas@gmail.com>
2016-12-16 13:12:20 -0700

    completed assignment

--- solution-mod-02/js/app.js
+++ solution-mod-02/js/app.js
@@ -2,13 +2,86 @@
 'use strict';
 
 angular.module('ShoppingListCheckoff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  .controller('ShoppingListCheckoffController', ShoppingListCheckoffController);
+function ShoppingListCheckOffService() {
+  var svc = this;
+  var list = [
+    {
+      name: 'cookies',
+      quantity: 10
+    },
+    {
+      name: 'chips',
+      quantity: '2 bags'
+    },
+    {
+      name: 'beers',
+      quantity: 12
+    },
+    {
+      name: 'wine',
+      quantity: '2 bottles'
+    },
+    {
+      name: 'sugar',
+      quantity: '1 bag'
+    },
+  ];
+  var pantry = [];
 
-ShoppingListCheckoffController.$inject = ['$scope'];
+  svc.addItem = function (itemName, itemQty) {
+    var item = {
+      name: itemName,
+      quantity: itemQty
+    };
+    list.push(item);
+  };
 
-function ShoppingListCheckoffController($scope) {
-  
+  svc.buyItem = function (itemIdx) {
+    var item = list[itemIdx];
+    list.splice(itemIdx, 1);
+    pantry.push(item);
+  };
+
+  svc.getList = function () {
+    return list;
+  };
+
+  svc.getPantry = function () {
+    return pantry;
+  };
+}
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var list = this;
+
+  list.itemName = "";
+  list.itemQty = ""
+
+  list.addItem = function () {
+    ShoppingListCheckOffService.addItem(list.itemName, list.itemQty);
+  };
+
+  list.buyItem = function (itemIdx) {
+    ShoppingListCheckOffService.buyItem(itemIdx);
+  };
+
+  list.getItems = function() {
+    return ShoppingListCheckOffService.getList();
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var pantry = this;
+
+  pantry.getItems = function() {
+    return ShoppingListCheckOffService.getPantry();
+  };
 }
 
 })();
