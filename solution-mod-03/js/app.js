95a7b4631595df7fab12597a404aa01eea2d1506
Julie Farkas <julie.l.farkas@gmail.com>
2016-12-17 15:24:40 -0700

    assignment 3 complete

--- solution-mod-03/js/app.js
+++ solution-mod-03/js/app.js
@@ -1,24 +1,70 @@
 (function () {
 'use strict';
 
-angular.module('App', [])
-.controller('Controller', Controller)
-.service('Service', Service);
+angular.module('NarrowItDownApp', [])
+.constant('menuUrl', 'https://davids-restaurant.herokuapp.com/menu_items.json')
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
-function Service() {
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: "E",
+    templateUrl: 'templates/found-items.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'ctrl',
+    bindToController: true
+  };
+  return ddo;
+}
+function FoundItemsDirectiveController() {
+  // nothing to do here?
+}
+MenuSearchService.$inject = ['$http', 'menuUrl'];
+function MenuSearchService($http, menuUrl) {
   var svc = this;
-  var items;
-  svc.getItems = function () {
-    return items;
+  svc.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: 'GET',
+      url: menuUrl
+    }).then(function (result) {
+        // process result and only keep items that match
+        var foundItems = result.data.menu_items.filter(function(item){
+          return item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+        });
+        // return processed items
+        return foundItems;
+    });
   };
 }
 
-Controller.$inject = ['Service'];
-function Controller(Service) {
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
   var ctrl = this;
+  ctrl.searchTerm = '';
+  ctrl.found = [];
+  ctrl.nothingFound = false;
 
-  ctrl.getItems = function() {
-    return Service.getItems();
+  ctrl.getMatchedMenuItems = function () {
+    ctrl.found = [];
+    ctrl.nothingFound = false;
+    if (ctrl.searchTerm) {
+      MenuSearchService.getMatchedMenuItems(ctrl.searchTerm).then(function(result){
+        ctrl.found = result;
+        if (ctrl.found.length === 0) ctrl.nothingFound = true;
+      }).catch(function (err) {
+        console.log(err);
+      });
+    }
+    else ctrl.nothingFound = true;
+  };
+
+  ctrl.removeItem = function (itemIndex) {
+    ctrl.found.splice(itemIndex, 1);
   };
 }
 
